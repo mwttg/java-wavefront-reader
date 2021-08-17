a3e2d9e68359ceba0a27bde233ebecc0af1cec1f
Marco Wittig <Marco-Wittig@gmx.de>
2021-08-17 19:59:26 +0200

    Fix SonarCloud bug issue

--- src/main/java/io/github/mwttg/wavefront/transformer/Wavefront.java
+++ src/main/java/io/github/mwttg/wavefront/transformer/Wavefront.java
@@ -1,7 +1,31 @@
 package io.github.mwttg.wavefront.transformer;
 
+import java.util.Arrays;
+
 /**
  * The resulting data structure with float Arrays which can be used by OpenGL.
  */
 public record Wavefront(float[] vertices, float[] textureCoordinates, float[] normals) {
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Wavefront wavefront = (Wavefront) o;
+    return Arrays.equals(vertices, wavefront.vertices) &&
+        Arrays.equals(textureCoordinates, wavefront.textureCoordinates) &&
+        Arrays.equals(normals, wavefront.normals);
+  }
+
+  @Override
+  public int hashCode() {
+    int result = Arrays.hashCode(vertices);
+    result = 31 * result + Arrays.hashCode(textureCoordinates);
+    result = 31 * result + Arrays.hashCode(normals);
+    return result;
+  }
 }
